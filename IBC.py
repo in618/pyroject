bd5173400b8af88cdd060711a6d7bf8e2f189c6f
ILYES N <65829330+in618@users.noreply.github.com>
2021-01-09 15:53:09 -0800

    Add files via upload

---
+++ IBC.py
@@ -0,0 +1,21 @@
+import Autodesk
+from Autodesk.Revit import DB
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import Transaction
+
+doc = __revit__.ActiveUIDocument.Document
+
+sheets = DB.FilteredElementCollector(doc)\
+                   .OfCategory(DB.BuiltInCategory.OST_Sheets)\
+                   .WhereElementIsNotElementType()\
+                   .ToElements()
+
+ibcsheets = []
+
+for s in sheets:
+    param = s.LookupParameter("WM_SHT-Office").AsString()
+    if param == "IBC":
+        ibcsheets.append(s.Id)
+
+for s in ibcsheets:
+    print(s)
